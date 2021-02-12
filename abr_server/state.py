6e2804ae6b9ccee6b6e95f3fdb1954d305ea2cd3
Bridger Herman <herma582@umn.edu>
2021-02-12 08:25:23 -0600

    Add incremental state backups

--- abr_server/state.py
+++ abr_server/state.py
@@ -1,5 +1,9 @@
+import os
+import sys
 import jsonschema
 import json
+import time
+from jsondiff import diff, patch
 from copy import deepcopy
 from django.conf import settings
 from pathlib import Path
@@ -8,8 +12,24 @@ from threading import Lock
 SCHEMA_PATH = Path(settings.STATIC_ROOT).joinpath('schemas')
 STATE_SCHEMA = SCHEMA_PATH.joinpath('ABRSchema_0-2-0.json')
 
+BACKUP_LOCATIONS = {
+    'linux': Path('~/.config/abr/'),
+    'darwin': Path('~/Library/Application Support/abr'),
+    'win32': Path('~/AppData/LocalLow/abr'),
+}
+
+BACKUP_PATH = BACKUP_LOCATIONS[sys.platform] \
+    .joinpath('abr_backup.json') \
+    .expanduser()
+
 class State():
     def __init__(self):
+        # Make sure the backup location exists
+        if not BACKUP_PATH.exists():
+            os.makedirs(BACKUP_PATH.parent)
+            BACKUP_PATH.touch()
+        self.backup_path = BACKUP_PATH.resolve()
+
         with open(STATE_SCHEMA) as scm:
             self.state_schema = json.load(scm)
 
@@ -54,8 +74,14 @@ class State():
             self._set_path(self._pending_state, item_path, new_value)
         try:
             jsonschema.validate(self._pending_state, self.state_schema)
+
+            # If we've successfully validated the state, make a backup. Keep
+            # up to a certain amount of backups so we can undo if necessary
+            self.make_backup()
+
             with self._state_lock:
                 self._state = deepcopy(self._pending_state)
+
             return ''
         except jsonschema.ValidationError as e:
             path = '/'.join(e.path)
@@ -69,11 +95,30 @@ class State():
             root = sub_path_parts[0]
             rest = sub_path_parts[1:]
 
-            # TODO: assuming we're creating an object for now
+            # Assuming everything we're assigning will be an object
             if root not in sub_state:
                 sub_state[root] = {}
 
             self._set_path(sub_state[root], rest, new_value)
 
+    def make_backup(self):
+        '''
+            Save a backup of the state using json-diff to the backup file.
+            Discard backups more than 2 hours old. The diff is backwards-facing,
+            for instance if we've just added an impression to 'impressions', the
+            diff will be {delete: 'impressions'}.
+        '''
+        try:
+            with open(self.backup_path, 'r') as backup_file:
+                backup_json = json.load(backup_file)
+        except FileNotFoundError:
+            backup_json = {}
+
+        with self._state_lock:
+            state_diff = diff(self._pending_state, self._state, dump=True)
+        backup_json[time.time()] = json.dumps(state_diff)
+
+        with open(self.backup_path, 'w') as backup_file:
+            json.dump(backup_json, backup_file)
 
 state = State()
